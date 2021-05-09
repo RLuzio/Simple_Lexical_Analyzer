dbc94bf7fe27e12e7a73ad0186978593b94279a7
RLuzio <46633368+RLuzio@users.noreply.github.com>
2021-05-09 12:05:39 -0700

    Update Lex.cpp
    Updated with some changes

--- Lex.cpp
+++ Lex.cpp
@@ -7,15 +7,15 @@
 using namespace std;
 
 							  //State table is 22 wide
-							  //Accept = 0, Starting = 1, Letter = 2, Digit = 3, Separators = 4, Operators = 5, Comment = 6
-				              //  L, D, (, ), {, }, [, ], ,, ., :, ;, *, +, -, =, /, >, <, %, !, \s\n\t
-const int state_table[][22] = {	{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },		//Accepting (skip spaces)
-								{ 2, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 6, 1 },		//Starting state (ignore spaces)
-								{ 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },		//Letters
-								{ 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },		//Digits
-								{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },		//Separators
-								{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },		//Operators
-								{ 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 0, 6 }		//Comment
+						      //Accept = 0, Starting = 1, Letter = 2, Digit = 3, Separators = 4, Operators = 5, Comment = 6
+							  //  L, D, (, ), {, }, [, ], ,, ., :, ;, *, +, -, =, /, >, <, %, !, \s\n\t, $, _
+const int state_table[][24] = { { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },		//Accepting (skip spaces)
+								{ 2, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 6, 1, 999, 999 },		//Starting state (ignore spaces)
+								{ 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2 },		//Letters
+								{ 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 999, 999 },		//Digits
+								{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 999, 999 },		//Separators
+								{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 999, 999 },		//Operators
+								{ 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 0, 6, 999, 999 }		//Comment
 };
 //const char char_table[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 const string KEYW[] = { "int", "float", "bool", "True", "False", "if", "else", "then", "endif", "endelse", "while", "whileend", "do", "enddo", "for", "endfor", "STDinput", "STDoutput", "and", "or", "not" };
@@ -47,7 +47,7 @@ bool is_operator(char tok) {
 	return false;
 }
 
-int is_separator(char tok) {
+bool is_separator(char tok) {
 	if (isspace(tok)) {
 		return true;
 	}
@@ -60,25 +60,32 @@ int is_separator(char tok) {
 }
 
 int update_col(char t) {
-	if (t == '!')				{ return 20; }
-	else if (isalpha(t))		{ return 0; }
-	else if (isdigit(t))		{ return 1; }
-	else if (is_operator(t))	{ return 14; }
-	else if (is_separator(t))	{ return 3; }
+	int col = 100;
+
+	if (t == '!') { col = 20; }
+	else if (isalpha(t)) { col = 0; }
+	else if (isdigit(t)) { col = 1; }
+	else if (is_operator(t)) { col = 14; }
+	else if (is_separator(t)) { col = 3; }
+	else if (t == '$') { col = 22; }
+	else if (t == '_') { col = 23;  }
+
+	return col;
 }
 
-int main(int argc, char* argv[]) {
-	int row = 0;
-	bool in_comment = 0;
-	int state = 1, col = 21, prev_state = 1;
+int main(/*int argc, char* argv[]*/) {
+	int state = 1, row = 0, col = 21, prev_state = 1, table_size = 0;
 
 	vector<vector<string>> lex_table;
 	vector<string> tmp;
 
-	string tok, line, tmptok;
+	string tok, line, file;
 
+	cout << "Enter a file to lexically analyze: ";
+	cin >> file;
+		
 	ifstream fin;
-	fin.open(argv[1]);
+	fin.open(file);
 
 	if (!fin.is_open()) {
 		printf("Failed to open file. Please restart\n");
@@ -87,18 +94,40 @@ int main(int argc, char* argv[]) {
 	}
 
 	while (getline(fin, line)) {
+		//tok.clear();
+		//state = 1;
 		for (int cp = 0; cp < line.length(); ++cp) {
-
 			col = update_col(line[cp]);
+			if(col == 100) { 
+				cout << "INVALID CHARACTER FOUND: " << line[cp] << endl;
+				cout << "EXITING!" << endl;
+				return 0;
+			}
+
 			prev_state = state;
 			state = state_table[state][col];
 
-			if (state != 6 && state != 0) {
+			if (state == 999) {
+				cout << "INVALID CHARACTER FOUND: " << line[cp] << endl;
+				cout << "EXITING!" << endl;
+				return 0;
+			}
+
+			else if (state != 6 && state != 0) {
 				tok += line[cp];
 			}
 
+			if (state == 4) {
+				state = 0;
+				prev_state = 4;
+			}
+			else if (state == 5) {
+				state = 0;
+				prev_state = 5;
+			}
+
+
 			if (state == 0 && !tok.empty()) {
-				//=====================================================
 				//Is Keyword
 				if (is_keyword(tok)) {
 					lex_table.push_back(tmp);
@@ -133,15 +162,19 @@ int main(int argc, char* argv[]) {
 					lex_table[row].push_back("Identifier");
 					lex_table[row++].push_back(tok);
 				}
+
 				tok.clear();
 				state = 1;
-				--cp;
+
+				if (!isspace(line[cp]) && prev_state != 4 && prev_state != 5) {
+					--cp;
+				}
 			}
 		}
 	}
 
-	for (row = 0; row < lex_table.size(); ++row) {
-		std::cout << setw(15) << lex_table[row][0] << setw(10) << "=\t" << lex_table[row][1] << std::endl;
+	for (int i = 0; i < lex_table.size(); ++i) {
+		std::cout << setw(15) << lex_table[i][0] << setw(10) << "=\t" << lex_table[i][1] << std::endl;
 	}
 
 	fin.close();
